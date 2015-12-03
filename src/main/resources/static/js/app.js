d04e9a5053b19b8c3570fef7d1098c8e807193c0
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-12-03 17:15:08 +0100

    working on search engine

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -46,10 +46,20 @@ $(document).ready(function () {
     variableWidth: true
   });
    
+  var posts = new Bloodhound({
+	  datumTokenizer: Bloodhound.tokenizers.whitespace,
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  // url points to a json file that contains an array of country names, see
+	  // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+	  prefetch: 'posts.json'
+  });
 
-  var index = lunr(function () {
-    this.field('title', {boost: 10})
-  })
+  // passing in `null` for the `options` arguments will result in the default
+  // options being used
+  $('#search .typeahead').typeahead(null, {
+	name: 'posts',
+	source: posts
+  });
 
 
 
